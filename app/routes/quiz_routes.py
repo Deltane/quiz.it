174523e43163a30dcdc73f5e23cf3f0b061de35c
rayhannm <mrayha8@gmail.com>
2025-04-29 17:57:30 +0800

    created the take quiz route after generating the quiz

--- app/routes/quiz_routes.py
+++ app/routes/quiz_routes.py
@@ -1,7 +1,37 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, session, request, jsonify, render_template
 
-quiz_bp = Blueprint('quiz', __name__)
+quiz_routes = Blueprint('quiz_routes', __name__)
 
-@quiz_bp.route('/create-quiz')
-def create_quiz():
-    return render_template('create_quiz.html')
+@quiz_routes.route('/take_quiz')
+def take_quiz():
+    return render_template('take_quiz.html')
+
+@quiz_routes.route('/store_quiz', methods=['POST'])
+def store_quiz():
+    session['quiz'] = request.json['quiz']
+    session['current_question'] = 0
+    return '', 204
+
+@quiz_routes.route('/get_question/<int:question_index>', methods=['GET'])
+def get_question(question_index):
+    quiz = session.get('quiz', [])
+    if question_index < len(quiz):
+        question = quiz[question_index]
+        return jsonify({
+            'question': question['question'],
+            'options': question['options']
+        })
+    return jsonify({}), 404
+
+@quiz_routes.route('/submit_answer', methods=['POST'])
+def submit_answer():
+    data = request.json
+    question_index = data['questionIndex']
+    answer = data['answer']
+
+    # Store the user's answer (you can save it in the session or database)
+    if 'answers' not in session:
+        session['answers'] = {}
+    session['answers'][question_index] = answer
+
+    return '', 204
